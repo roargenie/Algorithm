2d3bed94808667aaed46c924c14ebf94f8e027fa
roargenie <lazyhoper987@gmail.com>
2023-07-03 18:06:31 +0900

    풀이

---
+++ Algorithm/PRG/LV.2/마법의 엘리베이터.swift
@@ -0,0 +1,28 @@
+//
+//  마법의 엘리베이터.swift
+//  Algorithm
+//
+//  Created by 이명진 on 2023/07/03.
+//
+
+import Foundation
+
+func solution(_ storey: Int) -> Int {
+    var storey = storey
+    var result = 0
+
+    while storey != 0 {
+        // 상승의 경우
+        if storey % 10 > 5 || (storey % 10 == 5 && (storey / 10) % 10 >= 5) {
+            result += 10 - (storey % 10)
+            // 앞자리 + 1
+            storey = (storey / 10) + 1
+        } else {
+            // 하강의 경우
+            result += storey % 10
+            storey = storey / 10
+        }
+    }
+
+    return result
+}
